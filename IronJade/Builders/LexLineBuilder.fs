fd452cd71f5ba625d50ab45f4a2df3436974361a
Jake Taylor <JakeTaylorPro@gmail.com>
2015-02-21 16:12:06 -0500

    fixed issue where pipped text was getting an extra space at the beggining

--- IronJade/Builders/LexLineBuilder.fs
+++ IronJade/Builders/LexLineBuilder.fs
@@ -7,6 +7,6 @@
             else
                 match line with
                 | Util.Prefix "doctype " rest -> DocType(rest)
-                | Util.Prefix "|" rest -> TextLine(rest)
+                | Util.Prefix "| " rest -> TextLine(rest)
                 | Util.Prefix "//" rest -> Tag(LexTagBuilder.buildCommentLexTag rest)
                 | _-> Tag(LexTagBuilder.buildLexTag line)
